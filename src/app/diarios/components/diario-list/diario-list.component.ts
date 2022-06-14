3d7817328b32dc76e4a60f386457781280979199
Luiz Henrique Burgos <henriquerburgos@gmail.com>
2022-06-14 11:34:32 -0300

    Implementação de GridList na page de listagem de usuario.

--- src/app/diarios/components/diario-list/diario-list.component.ts
+++ src/app/diarios/components/diario-list/diario-list.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { HotToastService } from '@ngneat/hot-toast';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Diario } from 'src/app/core/models/diario';
 import { DiariosService } from 'src/app/core/services/diarios/diarios.service';
 import { DiarioAddComponent } from '../diario-add/diario-add.component';
 import { DiarioEditComponent } from '../diario-edit/diario-edit.component';
 import { UserService } from 'src/app/core/services/user/user.service';
 import { DiarioDeleteComponent } from '../diario-delete/diario-delete.component';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-diario-list',
   templateUrl: './diario-list.component.html',
-  styleUrls: ['./diario-list.component.scss'],
+  styleUrls: ['./diario-list.component.scss',],
 })
 export class DiarioListComponent implements OnInit {
   allDiarios$?: Observable<Diario[]>;
@@ -23,16 +24,49 @@ export class DiarioListComponent implements OnInit {
     private dialog: MatDialog,
     private diariosService: DiariosService,
     private toast: HotToastService,
-    private userService: UserService
+    private userService: UserService,
+    private breakpointObserve: BreakpointObserver,
   ) {} 
 
+  responsivo = this.breakpointObserve.observe([
+    Breakpoints.XSmall,
+    Breakpoints.Small,
+    Breakpoints.Medium,
+    Breakpoints.Large,
+    Breakpoints.XLarge
+  ]).pipe(
+    map((matches:BreakpointState) => {
+      if (matches.breakpoints[Breakpoints.XSmall]) { 
+        console.log("estou no xsmall")
+        return [
+          
+          { colunas: 2, linhas: 1, },
+        ]
+      }
+      if (matches.breakpoints[Breakpoints.Small]) { 
+        console.log("estou no small")
+        return [
+          { colunas: 2, linhas: 1, },
+        ]
+      }
+      if (matches.breakpoints[Breakpoints.Medium]) { 
+        console.log("estou no medium")
+        return [        
+          { colunas: 2, linhas: 1, },
+        ]
+      }
+      console.log("estou no small")
+      return [
+        {colunas: 1, linhas: 1, },
+      ]
+    })
+  );
+
   onClickAdd() {
-    
-    const ref = this.dialog.open(DiarioAddComponent, { maxWidth: '512px' });
-    
-    ref.afterClosed().subscribe({
+       const ref = this.dialog.open(DiarioAddComponent, { maxWidth: '512px' });
+      ref.afterClosed().subscribe({
       next: (result) => {
-        
+       
         if (result) {
           this.diariosService
             .addDiario(result.diario, result.imagem)
@@ -50,8 +84,7 @@ export class DiarioListComponent implements OnInit {
   }
 
   onClickEdit(diario: Diario) {
-    
-    const ref = this.dialog.open(DiarioEditComponent, {
+        const ref = this.dialog.open(DiarioEditComponent, {
       maxWidth: '512px',
       data: { ...diario },
     });
